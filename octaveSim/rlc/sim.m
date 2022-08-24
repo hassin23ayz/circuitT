28431bacc882db95bcd04fde03b96d95e69cf55c
ayz03 <hassin23ayz@gmail.com>
2022-08-24 20:55:37 +0600

    labels added

--- octaveSim/rlc/sim.m
+++ octaveSim/rlc/sim.m
@@ -1,4 +1,4 @@
-# this octave template file drives model.m
+ # this octave template file drives model.m
 clear  # clear all variables 
 clc    # clear screen 
 #--------------------------------------------
@@ -8,7 +8,7 @@ tstep=1e-6;    #step time,1us
 tfinal=100e-3; #end time, 1ms
 resume=0;      #set to 1 to resume simulation from last run
 
-dispcolors=['k';'b';'r';'g';'c';'k';'m']; # black, blue, red 
+dispcolors=['k;iL;';'b;vc;';'r;vs;';'g';'c';'k';'m']; # black, blue, red 
 
 # state variable is a derivative of a scope variable/vector such as d/dl(iL) = iLdot 
 statevars="[x]"; #state variables , output of the function , the function outputs array of state variables 
