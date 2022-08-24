67619356c8ec012888e2c9eddca5d1c208f5fcb8
hassin23ayz <hassin23ayz@gmail.com>
2022-08-24 15:48:38 +0600

    simulation related comments added

--- octaveSim/rlc/model.m
+++ octaveSim/rlc/model.m
@@ -1,11 +1,27 @@
 # this octave template file is driven by sim.m
-function xdot=model(x,t,params)
+function xdot=model(s,t,params) # s = simulation step driver , t for time 
 #Equation based Model 
 #
 tupdate(t);
 #----------------------------------------------
 # Type in Dynamic model equations here
-# other model functions get called here 
-#----------------------------------------------
-scopeit(1,[]); # give a list of variables in the array[] to plot 
+    iL = s(1);   # s(1) is a simulation step dependent value getting assigned to a vector                 
+    vc = s(2);   # s(2) is a simulation step dependent value getting assigned to a vector
+
+    vs = 10*(t>10e-3 && t<60e-3); # preparing the source  
+    L  = 10e-3;
+    R1 = 1;
+    R2 = 1000;
+    C  = 100e-6;
+
+    iLdot = (vs-(iL*R1)-vc)/L;  # iLdot is a state variable ,  derivative of vector iL 
+    vcdot = (iL/C) - (vc/R2)/C; # vcdot is a state variable ,  derivative of vector vc
+    xdot  = [iLdot;vcdot];      # derivates of state variables; this is the output of the function 
+#----------------------------------------------;
+scopeit(1,[vs]); # give a list of variables in the array[] to plot , here only scoped variables/vectors can be put 
 endfunction
+
+# run in octave
+# >> clc
+# >> pkg load luhelper_sim
+# >> sim 
--- octaveSim/rlc/sim.m
+++ octaveSim/rlc/sim.m
@@ -8,8 +8,10 @@ tstep=1e-6;    #step time,1us
 tfinal=100e-3; #end time, 1ms
 resume=0;      #set to 1 to resume simulation from last run
 
-dispcolors=['k';'b';'r';'g';'c';'k';'m'];
-statevars="[x]"; #state variables
+dispcolors=['k';'b';'r';'g';'c';'k';'m']; # black, blue, red 
+
+# state variable is a derivative of a scope variable/vector such as d/dl(iL) = iLdot 
+statevars="[x]"; #state variables , output of the function , the function outputs array of state variables 
 scopevars="[y]"; #scoped variables 
 #--------------------------------------------
 octsim           #run the simulation and will plot
